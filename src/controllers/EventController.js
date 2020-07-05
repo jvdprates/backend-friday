1d70aefacc60c0ec324bdd0038e05230851371aa
Arthur Martins Braga <arthurmbraga1@gmail.com>
2020-07-05 06:51:33 -0300

    feat: change events

--- src/controllers/EventController.js
+++ src/controllers/EventController.js
@@ -1,59 +1,79 @@
-const BarModel = require('../models/EventModel');
-const EventModel = require('../models/EventModel');
+const BarModel = require("../models/EventModel");
+const EventModel = require("../models/EventModel");
 
 module.exports = {
-    async create(request, response) {
-        try {
-            let event = request.body;
-            const result = await EventModel.createEvent(event);
-            return response.status(200).json(result);
-        } catch(err) {
-            console.log("Event creation failed: " + err);
-            return response.status(500).json({ notification: "Internal server error while trying to create event" });
-        }
-    },
-
-    async getAll(request, response) {
-        try {
-            const result = await EventModel.getAllEvents();
-            return response.status(200).json(result);
-        } catch(err) {
-            console.log("Event reading failed: " + err);
-            return response.status(500).json({ notification: "Internal server error while trying to get events" });
-        }
-    },
-
-    async getOne(request, response) {
-        try {
-            const { id } = request.params;
-            const result = await EventModel.getOneEvent(id);
-            return response.status(200).json(result);
-        } catch(err) {
-            console.log("Event reading failed: " + err);
-            return response.status(500).json({ notification: "Internal server error while trying to get one event" });
-        }
-    },
-
-    async update(request, response) {
-        try {
-            let { id } = request.params;
-            let event = request.body;
-            const result = await EventModel.updateEvent(id, event);
-            return response.status(200).json(result);
-        } catch (err) {
-            console.log("Event updating failed: " + err);
-            return response.status(500).json({ notification: "Internal server error while trying to update event" });
-        }
-    },
-
-    async delete(request, response){
-        try {
-            const { id } = request.params;
-            const result = await EventModel.deleteEvent(id);
-            return response.status(200).json(result);
-        } catch(err) {
-            console.log("Event deletion failed: " + err);
-            return response.status(500).json({ notification: "Internal server error while trying to delete Event" });
-        }
+  async create(request, response) {
+    try {
+      let event = request.body;
+      const result = await EventModel.createEvent(event);
+      return response.status(200).json(result);
+    } catch (err) {
+      console.log("Event creation failed: " + err);
+      return response
+        .status(500)
+        .json({
+          notification: "Internal server error while trying to create event",
+        });
     }
-}
+  },
+
+  async getAll(request, response) {
+    try {
+      const result = await EventModel.getAllEvents();
+      return response.status(200).json(result);
+    } catch (err) {
+      console.log("Event reading failed: " + err);
+      return response
+        .status(500)
+        .json({
+          notification: "Internal server error while trying to get events",
+        });
+    }
+  },
+
+  async getOne(request, response) {
+    try {
+      const { id } = request.params;
+      const result = await EventModel.getOneEvent(id);
+      return response.status(200).json(result);
+    } catch (err) {
+      console.log("Event reading failed: " + err);
+      return response
+        .status(500)
+        .json({
+          notification: "Internal server error while trying to get one event",
+        });
+    }
+  },
+
+  async update(request, response) {
+    try {
+      let { id } = request.params;
+      let event = request.body;
+      const result = await EventModel.updateEvent(id, event);
+      return response.status(200).json(result);
+    } catch (err) {
+      console.log("Event updating failed: " + err);
+      return response
+        .status(500)
+        .json({
+          notification: "Internal server error while trying to update event",
+        });
+    }
+  },
+
+  async delete(request, response) {
+    try {
+      const { id } = request.params;
+      const result = await EventModel.deleteEvent(id);
+      return response.status(200).json(result);
+    } catch (err) {
+      console.log("Event deletion failed: " + err);
+      return response
+        .status(500)
+        .json({
+          notification: "Internal server error while trying to delete Event",
+        });
+    }
+  },
+};
--- src/models/EventModel.js
+++ src/models/EventModel.js
@@ -1,46 +1,52 @@
 const connection = require("../database/connection");
 
 module.exports = {
-    async createEvent(event) {
-        console.log("Creating event: " + event.name);
-        const result = await connection("events")
-            .insert(event);
-        console.log("Event Created!");
-        return result;
-    },
+  async createEvent(event) {
+    console.log("Creating event: " + event.name);
+    const result = await connection("events").insert(event);
+    console.log("Event Created!");
+    return result;
+  },
 
-    async getOneEvent(event_id) {
-        console.log("Getting one event...");
-        const result = await connection("events as e")
-            .where("e.id", "=", event_id)
-            .join("bars as b", "e.bars_id", "=", "b.id")
-            .select("*", "e.description as event_description")
-            .first();
-        return result;
-    },
+  async getOneEvent(event_id) {
+    console.log("Getting one event...");
+    const result = await connection("events as e")
+      .where("e.id", "=", event_id)
+      .join("bars as b", "e.bars_id", "=", "b.id")
+      .select("*", "e.description as event_description")
+      .first();
+    return result;
+  },
 
-    async getAllEvents() {
-        console.log("Getting all events...");
-        const result = await connection("events")
-            .select("*")
-        return result;
-    },
+  async getAllEvents() {
+    console.log("Getting all events...");
+    const result = await connection("events AS e")
+      .join("bars AS b", "e.bars_id", "b.id")
+      .select(
+        "e.img_link AS event_image_link",
+        "e.category AS event_category",
+        "e.id AS event_id",
+        "e.description AS event_description",
+        "b.*"
+      );
+    return result;
+  },
 
-    async updateEvent(event_id, event) {
-        console.log("Updating event: " + event.name);
-        const result = await connection("events")
-            .where("id", "=", event_id)
-            .update(event);
-        console.log("Event Created!");
-        return result;
-    },
+  async updateEvent(event_id, event) {
+    console.log("Updating event: " + event.name);
+    const result = await connection("events")
+      .where("id", "=", event_id)
+      .update(event);
+    console.log("Event Created!");
+    return result;
+  },
 
-    async deleteEvent(event_id) {
-        console.log("Deleting event...");
-        const result = await connection("events")
-            .where("id", "=", event_id)
-            .delete();
-        console.log("Event Deleted!");
-        return result;
-    },
-}
+  async deleteEvent(event_id) {
+    console.log("Deleting event...");
+    const result = await connection("events")
+      .where("id", "=", event_id)
+      .delete();
+    console.log("Event Deleted!");
+    return result;
+  },
+};
